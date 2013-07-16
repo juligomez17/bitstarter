545444d330df928818b434ec9ca9423eb33aee75
Juliana Gomez <juligomez17@gmail.com>
2013-07-16 03:22:49 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('fs'); 
 
 var app = express.createServer(express.logger());
 var file = fs.readFileSync('index.html');
 var stringFile = file.toString(); 
 app.get('/', function(request, response) {
-  response.send('Hello World2');
+  response.send(stringFile);
 });
 
 var port = process.env.PORT || 5000;
